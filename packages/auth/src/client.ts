3d3b42bb4fd4a72486fbc2371259340f70f386b7
jafsparrow <jafarcees@gmail.com>
2026-07-09 08:20:13 +0400

    feat(auth, db): integrate admin client plugin and add dotenv-cli dependency

--- packages/auth/src/client.ts
+++ packages/auth/src/client.ts
@@ -1,10 +1,13 @@
 import { createAuthClient } from "better-auth/react"
+import { adminClient } from "better-auth/client/plugins"
 export const authClient = createAuthClient({
   /** The base URL of the server (optional if you're using the same domain) */
   baseURL: "http://localhost:3000",
+  plugins: [adminClient()],
   advanced: {
     crossOriginCookies: true,
   },
 })
 
+// Explicit export of some functions for convenience
 export const { signIn, signOut, signUp } = authClient
